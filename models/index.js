81a098a21ae296d87cad9a96af0f25e9321a3875
chebc <chebc@student.unimelb.edu.au>
2021-08-17 23:35:58 +0800

    test collection

--- models/index.js
+++ models/index.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 const mongoose = require("mongoose")
 
 // Connect to MongoDB - database login is retrieved from environment variables - YOU SHOULD USE YOUR OWN ATLAS CLUSTER
-CONNECTION_STRING = "mongodb+srv://bo:304068@cluster0.sgaws.mongodb.net/IT?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://bo:304068@cluster0.sgaws.mongodb.net/IT.Authors?retryWrites=true&w=majority"
 MONGO_URL = CONNECTION_STRING.replace("chebc@student.unimelb.edu.au",process.env.MONGO_USERNAME).replace("qqq789www=",process.env.MONGO_PASSWORD)
 
-mongoose.connect(MONGO_URL || "mongodb://localhost", { useNewUrlParser: true,
-useCreateIndex: true,
-useUnifiedTopology: true,
-useFindAndModify: false,
-dbName: "IT" 
+mongoose.connect(MONGO_URL || "mongodb://localhost", { 
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    dbName: "IT" 
 })
 
 const db = mongoose.connection
